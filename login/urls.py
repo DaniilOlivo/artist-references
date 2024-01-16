fb5c07c98ff509d0e39c544e75aad6b49265a4bd
Daniil <mister.danial.olivo@gmail.com>
2024-01-17 02:33:48 +0300

    Creating login and registration pages

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, re_path
-from login.views import LoginPageView
+from django.urls import path
+from login.views import LoginPageView, RegisterPageView
 
 urlpatterns = [
     path("login/", LoginPageView.as_view(), name="login"),
+    path("register/", RegisterPageView.as_view(), name="register"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,4 +1,15 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
 
 class LoginPageView(LoginView):
+    extra_context = {"active_section": "login", "part": "login"}
     template_name = 'login/login.html'
+
+class RegisterPageView(CreateView):
+    extra_context = {"active_section": "register", "part": "login"}
+    template_name = 'login/register.html'
+    model = User
+    success_url = "/auth/login/"
+    form_class = UserCreationForm
